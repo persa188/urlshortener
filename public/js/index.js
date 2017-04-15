7f0b3f1a171c42900a127151d06224378a8fe7c7
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-15 19:44:39 -0400

    revert

--- public/js/index.js
+++ public/js/index.js
@@ -6,11 +6,11 @@ var view = (function(){
   document.getElementById('shortener').onsubmit = function(e) {
     e.preventDefault();
     var url = document.getElementById('long-url').value;
-    console.log(url);
+    if (!/^(f|ht)tps?:\/\//i.test(url)) {
+      url = "http://" + url;
+    }
     doAjax('POST', 'https://url.sanic.ca/api/shorten/', {url: url}, true, function (err, data) {
       if (err) console.log(err);
-      console.log("hiii");
-      console.log(data);
       document.getElementById('result').innerHTML = `
       new url: https://url.sanic.ca/u/${data.short_url}`;
     });
