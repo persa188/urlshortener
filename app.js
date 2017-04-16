c62cd75531f6c19a186583dfd80dd5803c44f2dd
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-16 04:13:07 -0400

    enabled cors from https://url.sanic.ca

--- app.js
+++ app.js
@@ -36,6 +36,26 @@ var config = {
         cert: certificate
 };
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect, can use wildcards and multiple sites
+    res.setHeader('Access-Control-Allow-Origin', 'https://url.sanic.ca');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/', function(req, res, next) {
   res.sendFile('/public/index.html');
 });
