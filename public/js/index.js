b6cf6049b843aa5c3b2bc880ffaf6402ccf58673
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-16 02:00:27 -0400

    some basic styling added

--- public/js/index.js
+++ public/js/index.js
@@ -7,12 +7,23 @@ var view = (function(){
   document.getElementById('shortener').onsubmit = function(e) {
     e.preventDefault();
     var url = document.getElementById('long-url').value;
+    //auto-add http prefix
     if (!/^(f|ht)tps?:\/\//i.test(url)) {
       url = "http://" + url;
     }
     var reqbody = {
       url: url
     };
+
+    if (!isUrlValid(url))
+    {
+      document.getElementById('result').innerHTML = `
+      invalid url format, url must end with . followed by a top level domain
+      (e.x. sanic.ca, google.com, wikipedia.org, https://github.com)
+      `;
+      return;
+    }
+
     fetch('https://'+server+'/api/shorten/', {
             method: 'POST',
             headers: {
@@ -25,15 +36,23 @@ var view = (function(){
           })
           .then((response) => response.json())
           .then((responseJson) => {
-            //@TODO refresh using same cstext (current search text)
             if (responseJson.short_url) responseJson.short_url = responseJson.short_url.replace(/"/g,"");
             document.getElementById('result').innerHTML = `
             shortened url: https://${server}/u/${responseJson.short_url}`;
           })
           .catch((error) => {
             console.error(error);
+            document.getElementById('result').innerHTML = error;
           });
   }
+
+  /*regex validation from
+  https://stackoverflow.com/questions/2723140/validating-url-with-jquery-without-the-validate-plugin
+  */
+  function isUrlValid(url) {
+      return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+  }
+
 
   return view;
 }());
