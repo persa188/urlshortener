a635f01f1f17133ca4899dabcd358c8cd6854942
Brandon Mowat <brandonmowat11@gmail.com>
2017-04-15 23:55:09 -0400

    removed commented out lines of deprecated ajax call

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,6 @@ var view = (function(){
     if (!/^(f|ht)tps?:\/\//i.test(url)) {
       url = "http://" + url;
     }
-    // doAjax('POST', 'https://url.sanic.ca/api/shorten/', {url: url}, true, function (err, data) {
-    //   if (err) console.log(err);
-    //   document.getElementById('result').innerHTML = `
-    //   new url: https://url.sanic.ca/u/${data.short_url}`;
-    // });
     var reqbody = {
       url: url
     };
