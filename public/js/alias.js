9d39bec65c754f92ae2013f5679da4771fc7b501
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-19 02:16:40 -0400

    bugfixes

--- public/js/alias.js
+++ public/js/alias.js
@@ -1,12 +1,19 @@
 var alias = (function(){
   'use strict';
   var alias = {};
-  var aliases = {
-    "xn--dk8hms.ws": "&#x1F389;&#x1F4AF;.ws"
-  };
+  var aliases = [{
+    host: "xn--dk8hms.ws",
+    alias: "&#x1F389;&#x1F4AF;.ws"
+  }];
 
   alias.getAlias = function (host) {
-    return aliases.host ? aliases.host : host;
+    var count;
+    for (count = 0; count < aliases.length; count++) {
+      if (aliases[count].host === host) {
+        return aliases[count].alias;
+      }
+    }
+    return host;
   };
 
   return alias;
