72df8e2d873ae2895d61f815d426d4e72ac84a1f
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-19 09:11:20 -0400

    added tests

---
+++ test/tests.js
@@ -0,0 +1,72 @@
+var url = require('../models/url'),
+  vars = require("./testvars.js"),
+  chai = require('chai'),
+  chaiHttp = require('chai-http'),
+  server = require('../app').app,
+  should = chai.should(),
+  request = require('supertest'),
+  agent = request(server);
+
+chai.use(chaiHttp);
+
+/* Our parent block */
+describe('URL API Tests', () => {
+  describe('/POST /api/shorten', () => {
+      /* before each test */
+      before((done) => {
+        done();
+      });
+
+      it('adding a properly formatted url should work', (done) => {
+        agent.post('/api/shorten/').type('json').send(vars.testurl1)
+          .end(function (err, res) {
+            res.should.have.status(200);
+            done();
+          });
+      });
+
+      it('adding an empty url should return bad request', (done) => {
+        agent.post('/api/shorten/').type('json').send(vars.testurl_empty)
+          .end(function (err, res) {
+            res.should.have.status(400);
+            done();
+          });
+      });
+
+      it('adding a url without http:// should return bad request', (done) => {
+        agent.post('/api/shorten/').type('json').send(vars.testurl_bad_formatting)
+          .end(function (err, res) {
+            res.should.have.status(400);
+            done();
+          });
+      });
+
+  }); // end descr (/POST /api/shorten)
+}); // end outer (URL API TESTS)
+
+describe('URL API Tests', () => {
+  describe('/GET /u/:id', () => {
+    /* before each test */
+    before((done) => {
+      agent.post('/api/shorten/').type('json').send(vars.testurl_google)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          // global.google_id = res.body.short_url;
+          done();
+        });
+    });
+
+    it('should return code 200', function(done) {
+      agent
+        .get('/u/'+global.google_id)
+        .expect(302)
+        .end(function(err, res){
+          if(err) {
+            done(err);
+          } else {
+            done();
+          }
+        });
+    });
+  });
+});
---
+++ test/testvars.js
@@ -0,0 +1,15 @@
+exports.testurl1 = {
+  "url": "http://test.com"
+};
+
+exports.testurl_bad_formatting = {
+  "url": "test.com"
+};
+
+exports.testurl_empty = {
+  "url": ""
+};
+
+exports.testurl_google = {
+  "url": "https://google.ca"
+};
