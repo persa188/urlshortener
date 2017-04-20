abae865061136668c6b551c238193852ac64020e
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-20 08:53:36 -0400

    use env vars so builds will pass properly

--- conf.js
+++ conf.js
@@ -1,8 +1,9 @@
-var username = 'app';
-var password = 'app';
+var username = process.env.mongo_username;
+var password = process.env.mongo_password;
+var uri = process.env.mongo_uri;
 //this is an example db, change to your own for production use
 var mlab = {
-  mongouri: "mongodb://" + username + ":" + password + "@ds111771.mlab.com:11771/urlshortener-example"
+  mongouri: "mongodb://" + username + ":" + password + mongo_uri
 };
 
 module.exports = {mlab};
