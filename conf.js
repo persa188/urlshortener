ae21eabed6d58eb6f796f284b4c5541d2f08c4ae
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-20 07:53:07 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
-var username = 'brandon';
-var password = 'brandon';
+var username = 'app';
+var password = 'app';
 
 var mlab = {
   mongouri: "mongodb://" + username + ":" + password + "@ds161630.mlab.com:61630/urlshortener"
