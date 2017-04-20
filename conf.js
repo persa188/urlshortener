f80bd3e60002860a10ad169c588838c6dc984e91
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-20 08:54:42 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -3,7 +3,7 @@ var password = process.env.mongo_password;
 var uri = process.env.mongo_uri;
 //this is an example db, change to your own for production use
 var mlab = {
-  mongouri: "mongodb://" + username + ":" + password + mongo_uri
+  mongouri: "mongodb://" + username + ":" + password + "@" + mongo_uri
 };
 
 module.exports = {mlab};
