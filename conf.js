830c2c1d964908e8abe6a0720f1378e5d3b05bfc
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-20 09:29:50 -0400

    updated env vars to somthing more reasonable

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
-var username = process.env.mongo_username;
-var password = process.env.mongo_password;
-var uri = process.env.mongo_uri;
+var username = process.env.MONGO_USERNAME_URLSHORTENER;
+var password = process.env.MONGO_PASSWORD_URLSHORTENER;
+var uri = process.env.MONGO_URI_URLSHORTENER;
 //this is an example db, change to your own for production use
 var mlab = {
   mongouri: "mongodb://" + username + ":" + password + "@" + uri
