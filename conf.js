82115d4887a596e5a80ad1daf082a94d45dc9137
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-20 08:20:19 -0400

    changed to example db

--- conf.js
+++ conf.js
@@ -1,8 +1,8 @@
 var username = 'app';
 var password = 'app';
-
+//this is an example db, change to your own for production use
 var mlab = {
-  mongouri: "mongodb://" + username + ":" + password + "@ds161630.mlab.com:61630/urlshortener"
+  mongouri: "mongodb://" + username + ":" + password + "@ds111771.mlab.com:11771/urlshortener-example"
 };
 
 module.exports = {mlab};
