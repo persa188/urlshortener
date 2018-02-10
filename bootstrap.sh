5c9f12ec14e89c556576fb5e5e0257ffda5cf4fa
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2018-02-10 06:07:17 -0500

    added restart always flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 docker build -t url-shortener .
-docker run -d -p 7070:7070 -e MONGO_USERNAME_URLSHORTENER -e MONGO_PASSWORD_URLSHORTENER -e MONGO_URI_URLSHORTENER --name url-shortener url-shortener
+docker run -d -p 7070:7070 -e MONGO_USERNAME_URLSHORTENER -e MONGO_PASSWORD_URLSHORTENER -e MONGO_URI_URLSHORTENER --restart always --name url-shortener url-shortener
